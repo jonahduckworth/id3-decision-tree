1c8784bfa99407fc60d93ef1b725c3e81bf97bfe
jones <35585020+fleeced@users.noreply.github.com>
2022-01-03 20:26:54 -0700

    dataset file can read a .csv file and put it into a public static 2d array

--- dataset.java
+++ dataset.java
@@ -3,7 +3,44 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class dataset {
+    public static String[][] dataset = new String[14][5];
 
+    public static void getDataset() {
+        String line;
+        String splitBy = ",";
+        int i = 0;
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/dataset.csv"));
+
+            while ((line = br.readLine()) != null) {
+                String[] getData = line.split(splitBy);
+
+                for(int j = 0; j < 5; j++) {
+
+                    switch (getData[j]) {
+                        case "A+", "A", "A-", "B+", "B", "Yes", "No" -> {
+                            if(j == 0) {
+                                dataset[i][j] = getData[j];
+                            } else if(j == 1) {
+                                dataset[i][j] = getData[j];
+                            } else if(j == 2) {
+                                dataset[i][j] = getData[j];
+                            }  else if(j == 3) {
+                                dataset[i][j] = getData[j];
+                            } else {
+                                dataset[i][j] = getData[j];
+                            }
+                        }
+                    }
+
+                }
+
+                i++;
+            }
+
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
 
 }
--- driver.java
+++ driver.java
@@ -1,9 +1,11 @@
 public class driver {
 
     public static void main(String[] args) {
-
-
-
+        // get dataset from .csv file
+        dataset.getDataset();
+        System.out.println(dataset.dataset[3][3]);
     }
+
+
 
 }
