338ee0351fe7d3fa427958970cb6248c50cb7645
jones <35585020+fleeced@users.noreply.github.com>
2022-01-03 20:40:07 -0700

    calculate.getCategory counts yes and no's for each subject

--- calculate.java
+++ calculate.java
@@ -2,6 +2,21 @@ import java.util.Objects;
 
 public class calculate {
 
+    public static void getCategory(String grade, int subject) {
+        String[][] category = dataset.dataset;
+        dataset.count = 0;
 
+        for(int i = 0; i < dataset.dataset.length; i++) {
+
+            if(Objects.equals(category[i][subject], grade) && Objects.equals(category[i][4], "Yes")) {
+                dataset.yes++;
+                dataset.count++;
+            } else if(Objects.equals(category[i][subject], grade) && Objects.equals(category[i][4], "No")) {
+                dataset.no++;
+            }
+
+        }
+
+    }
 
 }
