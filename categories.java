32effd48cfaab373b6262febac19b75b27e1ac7a
jones <35585020+fleeced@users.noreply.github.com>
2022-01-03 21:09:03 -0700

    categories.getMath gets entropy for every grade and calls functions for information gain

--- categories.java
+++ categories.java
@@ -7,9 +7,27 @@ public class categories {
     }
 
     public static void getMath() {
+        // calculate entropy for every grade
         calculate.getCategory("A+", 0);
         dataset.ap = calculate.entropy();
         dataset.apCount = dataset.count;
+        calculate.getCategory("A", 0);
+        dataset.a = calculate.entropy();
+        dataset.aCount = dataset.count;
+        calculate.getCategory("A-", 0);
+        dataset.am = calculate.entropy();
+        dataset.amCount = dataset.count;
+        calculate.getCategory("B+", 0);
+        dataset.bp = calculate.entropy();
+        dataset.bpCount = dataset.count;
+        calculate.getCategory("B", 0);
+        dataset.b = calculate.entropy();
+        dataset.bCount = dataset.count;
+
+        // calculate average entropy
+        dataset.mathEntropy = calculate.averageEntropy();
+        // calculate information gain
+        dataset.mathEntropy = calculate.informationGain(dataset.mathEntropy);
     }
 
 }
